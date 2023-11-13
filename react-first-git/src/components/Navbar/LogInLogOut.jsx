4e92a402ebe71fbeed318cd39f2982b491317214
MD JASIM UDDIN <mjunachole97@gmail.com>
2023-11-14 01:16:08 +0600

    log in function

--- react-first-git/src/components/Navbar/LogInLogOut.jsx
+++ react-first-git/src/components/Navbar/LogInLogOut.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-const LoginLogOut = () => {
-    const isLogIn = true;
+const LoginLogOut = (props) => {
+    const {isLogIn, updateData} = props;
     return (
-        <div>
+        <Fragment>
             <div className="">
-                {isLogIn?(<div className="profile-icon">
+                {
+                isLogIn?(
+                    <div onClick={updateData} className="profile-icon">
                     <img src="https://scontent.fdac22-1.fna.fbcdn.net/v/t39.30808-6/337136839_5854203581368299_7969630753016429492_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEfVT3yrcqXknqM7zbMqwMd5Qf1wPK2U4flB_XA8rZTh5Zieu-baXUivKwIZb5nFmI3-FTlOocvwd_8GaihKZYE&_nc_ohc=cmoznjDakTYAX-15JLq&_nc_ht=scontent.fdac22-1.fna&oh=00_AfAkx_P-izpf81DPwDzGR_Xkrsj8vmqsUVUsclT2g76riA&oe=655700D6" alt="" />
                 </div>):(
-                <div className="logSign">
-                    <button className='login-btn'>Login</button>
-                    <button className='Sign-Up'>SignUp</button>
-                </div>)}
+                    <div className="logSign">
+                        <button onClick={updateData} className='login-btn'>Login</button>
+                        <button className='Sign-Up'>SignUp</button>
+                    </div>)
+                }
             </div>
-        </div>
+        </Fragment>
     );
 };
 
--- react-first-git/src/components/Navbar/Navbar.jsx
+++ react-first-git/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,20 @@
 
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './Navbar.css';
-import LoginLogOut from './LoginLogOut';
+import LogInLogOut from './LogInLogOut';
 const Navbar = () => {
+    const [isLogIn, setIsLogIn] = useState(false);
+
+    const update = ()=>{
+        setIsLogIn(!isLogIn)
+    }
+
     return (
         <Fragment>
             <nav>
                 <div className='Cname'>JTech</div>
-                <LoginLogOut/>
+                <LogInLogOut isLogIn={isLogIn}
+                updateData ={update}/>
             </nav>
         </Fragment>
     );
